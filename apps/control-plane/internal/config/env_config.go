03093be4efe8e4d86046b6a09116a3b4f92c29f7
João Paulo Gomes Rodrigues <jpgomesr07@gmail.com>
2026-03-19 19:40:25 -0300

    refactor: Update Config struct to enforce required fields for Keycloak and database variables

--- apps/control-plane/internal/config/env_config.go
+++ apps/control-plane/internal/config/env_config.go
@@ -9,35 +9,43 @@ import (
 )
 
 type Config struct {
-	Port           int      `env:"PORT" default:"8180"`
-	Env            string   `env:"ENV" default:"development"`
-	Issuer         string   `env:"ISSUER" default:""`
-	ClientId       string   `env:"CLIENT_ID" default:""`
-	ClientSecret   string   `env:"CLIENT_SECRET" default:""`
-	RedirectURL    string   `env:"REDIRECT_URL" default:""`
-	Scopes         []string `env:"SCOPES" default:"" split:","`
-	LogoutRedirect string   `env:"LOGOUT_REDIRECT_URL" default:""`
-	SessionSecret  string   `env:"SESSION_SECRET" default:""`
+	Port int    `env:"PORT" default:"8180"`
+	Env  string `env:"ENV" default:"development"`
+
+	// Keycloak vars
+	Issuer         string   `env:"ISSUER" required:"true"`
+	ClientId       string   `env:"CLIENT_ID" required:"true"`
+	ClientSecret   string   `env:"CLIENT_SECRET" required:"true"`
+	RedirectURL    string   `env:"REDIRECT_URL" required:"true"`
+	Scopes         []string `env:"SCOPES" required:"true" split:","`
+	LogoutRedirect string   `env:"LOGOUT_REDIRECT_URL" required:"true"`
+	SessionSecret  string   `env:"SESSION_SECRET" required:"true"`
+
+	// Database vars
+	Host     string `env:"DB_HOST" required:"true"`
+	Username string `env:"DB_USERNAME" required:"true"`
+	Password string `env:"DB_PASSWORD" required:"true"`
+	DbName   string `env:"DB_NAME" required:"true"`
 }
 
 var cfg Config
 
 // LoadConfig loads environment variables into cfg
 func LoadConfig() *Config {
-    env := os.Getenv("ENV")
-    if env == "" {
-        env = "development"
-    }
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "development"
+	}
 
-    if env != "production" {
-        filename := ".env." + env // → ".env.development"
-        if err := godotenv.Load(filename); err != nil {
-            log.Printf("No %s file found, relying on environment variables\n", filename)
-        }
-    }
+	if env != "production" {
+		filename := ".env." + env // → ".env.development"
+		if err := godotenv.Load(filename); err != nil {
+			log.Printf("No %s file found, relying on environment variables\n", filename)
+		}
+	}
 
-    if err := envconfig.Process("", &cfg); err != nil {
-        log.Fatalf("Failed to load env vars: %v", err)
-    }
-    return &cfg
+	if err := envconfig.Process("", &cfg); err != nil {
+		log.Fatalf("Failed to load env vars: %v", err)
+	}
+	return &cfg
 }
